9326d8663ba2173888589491402f9280de491103
krisziaova-spec <kriszia.ova@gmail.com>
2026-05-30 08:22:24 +0800

    Update UserRepository.java

--- src/main/java/com/pdts/repository/UserRepository.java
+++ src/main/java/com/pdts/repository/UserRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    Optional<User> findByUsername(String username);
+
     Optional<User> findByUsernameIgnoreCase(String username);
 
     boolean existsByUsername(String username);
